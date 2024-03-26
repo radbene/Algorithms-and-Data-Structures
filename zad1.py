2b82f28fed55de0a3240d33567d02845ce50ab0d
radbene <162505307+radbene@users.noreply.github.com>
2024-03-26 12:55:27 +0100

    Add files via upload
    Nodes of a one-way cross-reference list are represented in the form:
    
    class Node:
    def __init__(self):
    self.val = None # stored real number
    self.next = None # reference to the next element
    
    Let p be a pointer to a non-empty cross-reference list containing pairwise different real numbers a1, a2, . . . , an (the list has no guardian). We say that the list is k-chaotic if for each element it occurs that after sorting the list it would be in a position that differs from the current one
    by at most k. Thus, a 0-chaotic list is sorted, an example of a 1-chaotic list is
    1, 0, 3, 2, 4, 6, 5, and an (n - 1)-chaotic list of length n can contain numbers in any order.
    Please implement the function SortH(p,k), which sorts a k-chaotic list pointed to by p.
    The function should return a pointer to the sorted list. The algorithm should be as fast as possible
    and use as little memory as possible (in the asymptotic sense, measured against the length n of the list
    and parameter k)

---
+++ zad1.py
@@ -0,0 +1,31 @@
+from zad1testy import Node, runtests
+#Radosław Benedykciński
+#Operation of the algorithm - for each element, the algorithm searches through the next k elements (less if we run out of the list earlier)
+#the smallest of them is "clipped" to the beginning and then moves the pointer of the last element to the newly inserted element(to sort in ascending rather than descending order)
+#algorithm has time complexity O(nk) and memory coplexity of O(1)
+def SortH(p,k):
+    l = Node()  #guardian
+    l.next = p  #l becomes original list with a guardian for easy unclipping of first element
+    H = begin = Node() #Head of sorted list
+    H.val = H.next = None
+    while l.next:   #Loop that checks if there are any elements left in l(except for guardian)
+        i = 0   #counter for k next elements
+        p = l.next  #l is now head of original list so p can now travel through the list
+        mini = p.val
+        prev=l  #pointer for previous element
+        while p.next is not None and i < k: #Loop that searches for lowest value in k next elements(or until the end of a list)
+            if p.next.val < mini:
+                mini = p.next.val
+                prev = p
+            p = p.next
+            i += 1
+        x = prev.next   #unclipping element that has lowest value
+        prev.next = x.next
+        H.next = x  #clipping lowest value to the end of sorted list
+        x.next = None   #detaching x from l
+        H = H.next  #moving H to the newly clipped element(the current last element of the sorted list)
+    return begin.next   #returning a pointer to the first element of the sorted list(it had a guardian so it returns guardian.next)
+
+
+#change all_tests to True to run all tests
+runtests(SortH, all_tests = True )
